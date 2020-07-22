60df03dadfd4a4f513f8562d4d28cd2a0f792bff
思维空间 <dnyz520@126.com>
2020-07-22 16:45:49 +0800

    up

--- src/CaptchaController.php
+++ src/CaptchaController.php
@@ -1,15 +1,6 @@
 <?php
-// +----------------------------------------------------------------------
-// | ThinkPHP [ WE CAN DO IT JUST THINK ]
-// +----------------------------------------------------------------------
-// | Copyright (c) 2006-2015 http://thinkphp.cn All rights reserved.
-// +----------------------------------------------------------------------
-// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
-// +----------------------------------------------------------------------
-// | Author: yunwuxin <448901948@qq.com>
-// +----------------------------------------------------------------------
 
-namespace think\captcha;
+namespace careyshop\captcha;
 
 class CaptchaController
 {
