8a4e66812f7896ba78195e97012d052f6bb873ae
思维空间 <dnyz520@126.com>
2020-09-29 11:02:35 +0800

    no message

--- src/CaptchaService.php
+++ src/CaptchaService.php
@@ -13,7 +13,7 @@ class CaptchaService extends Service
         $this->registerRoutes(function (Route $route) {
             $route->get('info/captcha', function (Config $config) {
                 $data = $config->get('extra.product', []);
-                $data['conposer'] = 'captcha';
+                $data['conposer'] = 'Captcha';
                 $data['statement'] = base64_decode('5pys56uZ5oqA5pyv5Z+65LqOQ2FyZXlTaG9w5ZWG5Z+O5qGG5p6257O757uf');
 
                 foreach ($data as $key => $value) {
